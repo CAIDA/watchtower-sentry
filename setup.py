1850a5018946ee49bbc895ecfdef0a20a33d86f7
Alistair King <alistair@caida.org>
2016-08-11 10:17:32 -0700

    Re-add include_package_data to setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     url='http://github.com/caida/watchtower-sentry',
     packages=find_packages(),
     install_requires=install_requires,
+    include_package_data=True,
     entry_points={'console_scripts': [
         'watchtower-sentry=watchtower.sentry.app:run'
     ]},
