d6afd2fa0eb916e0db919def036e801806f006c1
yehzhang <simonzhangintp@gmail.com>
2016-11-30 10:40:49 -0800

    Accept scan_from and scan_until options from command line

--- watchtower/sentry/app.py
+++ watchtower/sentry/app.py
@@ -2,7 +2,7 @@ import signal
 
 from tornado.options import define, options
 
-from .core import Reactor, ScannerReactor
+from .core import Reactor
 
 
 define('config', default=Reactor.defaults['config'], help='Path to an configuration file (YAML)')
@@ -14,19 +14,6 @@ def run():
     options.parse_command_line()
 
     r = Reactor(**options.as_dict())
-
-    signal.signal(signal.SIGTERM, r.stop)
-    signal.signal(signal.SIGINT, r.stop)
-    if hasattr(signal, 'SIGHUP'):
-        signal.signal(signal.SIGHUP, r.reinit)
-
-    r.start()
-
-
-def run_scanner():
-    options.parse_command_line()
-
-    r = ScannerReactor(**options.as_dict())
 
     signal.signal(signal.SIGTERM, r.stop)
     signal.signal(signal.SIGINT, r.stop)
