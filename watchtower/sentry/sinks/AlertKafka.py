b1fb642a9e70998371e248fe5cd764368f3ff010
Ken Keys <kkeys@caida.org>
2019-09-18 17:06:49 -0700

    Implement rough version of AlertKafka module

---
+++ watchtower/sentry/sinks/AlertKafka.py
@@ -0,0 +1,124 @@
+"""Sink that detects extreme values and sends alert objects to a kafka cluster.
+
+Configuration parameters ('*' indicates required parameter):
+    fqid*: (string) Unique identifier for this data source.
+    name*: (string) Human-readable name for this data source.
+    method*: (string) Human-readable description of the sentry pipeline.
+    min: (number <1.0) Generate alert if value falls below this value.
+    max: (number >1.0) Generate alert if value rises above this value.
+    brokers*: (string) Comma-separated list of kafka brokers.
+    topicprefix*: (string) Kafka topic prefix.
+
+    At least one of {min} or {max} is required.
+
+Input:  (key, value, time)
+
+Sink result:  alert objects sent to kafka cluster.
+"""
+
+import json
+import logging
+import confluent_kafka
+import SentryModule
+
+logger = logging.getLogger(__name__)
+
+add_cfg_schema = {
+    "properties": {
+        "fqid":        {"type": "string"},
+        "name":        {"type": "string"},
+        "method":      {"type": "string"},
+        "min":         {"type": "number", "exclusiveMaximum": 1.0},
+        "max":         {"type": "number", "exclusiveMinimum": 1.0},
+        "brokers":     {"type": "string"},
+        "topicprefix": {"type": "string"},
+    },
+    "required": ["fqid", "name", "method", "brokers", "topicprefix"],
+    "oneOf": [{"required": ["min"]}, {"required": ["max"]}]
+}
+
+class AlertKafka(SentryModule.Sink):
+    def __init__(self, config, gen, ctx):
+        logger.debug("AlertKafka.__init__")
+        super().__init__(config, logger, gen)
+        self.fqid = config['fqid']
+        self.name = config['name']
+        self.method = config['method']
+        self.brokers = config['brokers']
+        self.topicprefix = config['topicprefix']
+        self.min = config.get('min', None)
+        self.max = config.get('max', None)
+        self.condition_label = [
+            "< %r" % self.min,   # -1
+            "normal",            # 0
+            "> %r" % self.max    # 1
+        ]
+
+        self.topic = "%s-alerts" % (self.topicprefix)
+        self.alert_status = dict()
+        kp_cfg = {
+            'bootstrap.servers': self.brokers,
+        }
+        self.kproducer = confluent_kafka.Producer(kp_cfg)
+        for k in ['expression', 'method']:
+            if k not in ctx:
+                raise RuntimeError('context "%s" was not set by any previous '
+                    'module' % k)
+
+    def run(self, ctx):
+        logger.debug("AlertKafka.run()")
+        for entry in self.gen(ctx):
+            logger.debug("AK: %s", str(entry))
+            key, value, t = entry
+            if key not in self.alert_status:
+                self.alert_status[key] = 0
+            if self.min is not None and value < self.min:
+                alert_status = -1
+            elif self.max is not None and value > self.max:
+                alert_status = 1
+            else:
+                alert_status = 0
+            if alert_status != self.alert_status[key]:
+                self.alert_status[key] = alert_status
+                # Cram our alert data into the watchtower-alert legacy format
+                record = {
+                    "fqid": self.fqid,
+                    "name": self.name,
+                    "level": "critical" if alert_status != 0 else "normal",
+                    "time": t,
+                    "expression": ctx['expression'],
+                    "history_expression": None,
+                    "method": ctx['method'],
+                    "violations": [{
+                        "expression": str(key, 'ascii'),
+                        "condition": self.condition_label[alert_status+1],
+                        "value": value,
+                        "history_value": None,
+                        "history": None,
+                        "time": t,
+                    }],
+                }
+
+                # Trigger any available delivery report callacks from previous
+                # produce() calls
+                self.kproducer.poll(0)
+                # Asynchonously produce a message.  The delivery report
+                # callback will be triggered from poll() above, or flush()
+                # below, when the message has been successfully delivered or
+                # failed permanently.
+                msg = json.dumps(record, separators={',',':'})
+                self.kproducer.produce(self.topic,
+                    value=bytes(msg, 'ascii'),
+                    key=key,
+                    on_delivery=self.kp_delivery_report)
+                print(json.dumps(record))
+
+        self.kproducer.flush()
+        logger.debug("AlertKafka.run() done")
+
+    def kp_delivery_report(self, err, msg):
+        if err is not None:
+            logger.error("message delivery failed: {}".format(err))
+        else:
+            logger.debug("message delivered to {} [{}]".format(
+                msg.topic(), msg.partition()))
